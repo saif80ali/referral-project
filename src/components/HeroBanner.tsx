3aae00823789c47a5c194664d970046b9754d713
saif80ali <mdsaif80ali@gmail.com>
2024-08-25 21:48:49 +0530

    adding navbar

---
+++ src/components/HeroBanner.tsx
@@ -0,0 +1,8 @@
+export default function HeroBanner() {
+  return (
+    <div className="mt-2 container text-center non-loggedin-hero-banner">
+      <h1 className="heading-logo-font fs-1">Land your dream job <span className="text-primary">3.5x</span><br></br>faster with a referral</h1>
+      <button type="button" className="btn btn-primary btn-lg">Try now</button>
+    </div>
+  )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,14 @@ import '../style/Navbar.scss';
 
 export default function Navbar() {
   return (
-    <div>
-      Hello world
-    </div>
-  )
+    <nav className="navbar border-body">
+      <div className="container">
+        <span className="mb-0 fs-1 heading-logo-font">ReferMe</span>
+        <div className="d-flex gap-2" role="search">
+          <button type="button" className="btn btn-outline-dark border-0">Sign in</button>
+          <button type="button" className="btn btn-success">Sign up</button>
+        </div>
+      </div>
+    </nav>
+  );
 }
