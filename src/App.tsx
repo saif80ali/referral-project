901b3c48c6bbdead02bb2f53ab7b52a0eff9d671
saif80ali <mdsaif80ali@gmail.com>
2024-09-17 22:47:17 +0530

    Adding my profile page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import './App.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './store/store.ts';
 import { Provider } from 'react-redux'
-import { Route, Routes } from 'react-router-dom';
-import { Toaster } from './components/Toaster.tsx';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Toaster } from './components/Toaster';
 import { LoginModal } from './modal/LoginModal';
-import Header from './components/Header.tsx'
-import HeroBanner from './components/HeroBanner';
+import Header from './components/Header'
+import Dashboard from './components/Dashboard';
 import Transactions from './components/Transactions';
-import NewEntryForm from './components/NewEntryForm.tsx';
-import About from './components/About.tsx';
-import NotesDashboard from './components/NotesDashboard.tsx';
-import Footer from './components/Footer.tsx';
-import Loader from './components/Loader.tsx';
+import NewEntryForm from './components/NewEntryForm';
+import About from './components/About';
+import NotesDashboard from './components/NotesDashboard';
+import Footer from './components/Footer';
+import Loader from './components/Loader';
+import PageNotFound from './components/PageNotFound';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
@@ -20,11 +22,15 @@ function App() {
       <Loader></Loader>
       <Header></Header>
       <Routes>
-        <Route path="/" element={<HeroBanner/>}/>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="home" element={<Navigate to="/" replace />} />
         <Route path="about" element={<About/>}/>
         <Route path='notes' element={<NotesDashboard/>}/>
         <Route path="transactions" element={<Transactions/>}/>
         <Route path="new-entry" element={<NewEntryForm/>}/>
+        <Route path="sign-up" element={<MyProfile/>}/>
+        <Route path="profile" element={<MyProfile/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
       </Routes>
       <Footer></Footer>
       <LoginModal></LoginModal>
