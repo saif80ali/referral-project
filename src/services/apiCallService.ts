739b628819bff7d401a45a0fb64681af4b471cbd
saif80ali <mdsaif80ali@gmail.com>
2024-11-13 00:20:13 +0530

    Common endpoint of API

--- src/services/apiCallService.ts
+++ src/services/apiCallService.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+const baseURL = (import.meta.env.VITE_BACKEND_URL || "http://localhost:5000") + "/api/";
+
 
 const getMethod = (endpoint:string) => {
-    return axios.get(baseURL + "/api/" + endpoint, {
+    return axios.get(baseURL + endpoint, {
         headers: {
           'auth-token': localStorage.getItem('token'),
         }
