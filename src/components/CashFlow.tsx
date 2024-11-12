bf2473359611cbfaff9284e30a5d88433b62f716
saif80ali <mdsaif80ali@gmail.com>
2024-11-12 23:42:14 +0530

    width 100% class correction

--- src/components/CashFlow.tsx
+++ src/components/CashFlow.tsx
@@ -4,7 +4,7 @@ import siteUnderMaintainance from "../assets/under-maintaince-page.svg"
 export default function CashFlow() {
   return (
     <div className="container">
-      <img className="w-100% maintainance-image" src={siteUnderMaintainance} />
+      <img className="w-100 maintainance-image" src={siteUnderMaintainance} />
       <h3 className="text-center">This page is under development</h3>
     </div>
   );
